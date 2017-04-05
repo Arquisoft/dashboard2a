a0d7d53483f82a3a157315f2e92978ebc73f7796
RubenGarciaR <rubi_ruben_22@hotmail.com>
2017-04-05 18:57:06 +0200

    Prueba con selenium

--- src/test/java/uo/asw/steps/LoginUserSteps.java
+++ src/test/java/uo/asw/steps/LoginUserSteps.java
@@ -12,23 +12,23 @@ import org.springframework.boot.test.SpringApplicationContextLoader;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Dado;
 import cucumber.api.java.es.Entonces;
 import uo.asw.Application;
 import uo.asw.dashboard.MainController;
+import uo.asw.selenium.RellenarFormularioLogin;
 import uo.asw.utils.SauceUtils;
+import uo.asw.utils.SeleniumUtils;
 
-import java.util.List;
+
 
 @ContextConfiguration(classes = Application.class, loader = SpringApplicationContextLoader.class)
 @IntegrationTest
 @WebAppConfiguration
 public class LoginUserSteps {
 
-	private static final Logger LOG = LoggerFactory.getLogger(MainController.class);
 
 	private WebDriver driver;
 
@@ -40,17 +40,17 @@ public class LoginUserSteps {
 
 	@Dado("^un formulario de login$")
 	public void un_formulario_de_login() throws Throwable {
-		
+		SeleniumUtils.textoPresentePagina(driver, "Inicio de sesion");
 	}
 
 	@Cuando("^me logueo como usuario \"([^\"]*)\" con password \"([^\"]*)\"$")
-	public void me_logueo_como_usuario_con_password(String arg1, String arg2) throws Throwable {
-		
+	public void me_logueo_como_usuario_con_password(String user, String password) throws Throwable {
+		new RellenarFormularioLogin().rellenaFormulario(driver, user, password);
 	}
 
 	@Entonces("^voy al dashboard del alcalde$")
 	public void voy_al_dashboard_del_alcalde() throws Throwable {
-		
+		SeleniumUtils.textoPresentePagina(driver, "Pagina del usuario");
 	}
 
 }

9bbcf3df4ff7aaf21c4a7bb635cb5e47539ad9cc
Sergio Flórez <sergiozerolf@gmail.com>
2017-04-07 01:44:24 +0200

    Revert "tests problematicos convertidos a tests cucumber exitosos"
    This reverts commit 73265886b5eb43824b19892aa605907404d201f5.

---
+++ src/test/java/uo/asw/selenium/dashboard/AutenticationTest.java
@@ -0,0 +1,88 @@
+package uo.asw.selenium.dashboard;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.SpringApplicationContextLoader;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import uo.asw.Application;
+import uo.asw.utils.SauceUtils;
+import uo.asw.utils.SeleniumUtils;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = Application.class, loader = SpringApplicationContextLoader.class)
+public class AutenticationTest {
+
+//    private web
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = SauceUtils.getDriver();
+        baseUrl = "http://localhost:8090/";
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void successful() throws Exception {
+        driver.get(baseUrl + "/");
+        driver.navigate().to("http://localhost:8090/");
+        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        SeleniumUtils.textoPresentePagina(driver,
+                "Sistema de participacion ciudadana");
+        driver.findElement(By.id("userInput")).sendKeys("alcalde");
+        driver.findElement(By.id("passwdInput")).sendKeys("1234");
+        driver.findElement(By.id("btnEnviar")).click();
+        try {
+            assertEquals("Dashboard", driver.findElement(By.className("title")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+
+    @Test
+    public void unsuccessful() throws Exception {
+        driver.get(baseUrl + "/");
+        SeleniumUtils.textoPresentePagina(driver,
+                "Sistema de participacion ciudadana");
+        driver.findElement(By.id("userInput")).sendKeys("alcalde");
+        driver.findElement(By.id("passwdInput")).sendKeys("alcalde");
+        driver.findElement(By.id("btnEnviar")).click();
+        SeleniumUtils.textoPresentePagina(driver,
+                "Error: no se encuentra el usuario");
+        driver.findElement(new By.ByLinkText("Volver a inicio")).click();
+        SeleniumUtils.textoPresentePagina(driver,
+                "Sistema de participacion ciudadana");
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+}
+
--- src/test/resources/features/loginErroneo.feature
+++
@@ -1,8 +0,0 @@
-# language: es
-Característica: Login de usuario
-
-  Escenario: Login como alcalde
-
-    Dado el formulario de registro de la pagina principal
-    Cuando me logueo como usuario que no existe
-    Entonces se me muestra un error
